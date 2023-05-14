7d8e47717e20d26ae4c68ac7d4f340ea9cdbbcdb
Ivan Panchenko <wao@mail.ru>
2023-05-14 09:25:11 +0300

    Slightly improved tests

--- sql/knn.sql
+++ sql/knn.sql
@@ -5,6 +5,8 @@ SET enable_indexscan = true;
 EXPLAIN (costs off) SELECT p <-> spoint (0.2, 0.3) FROM points ORDER BY 1 LIMIT 10;
 UPDATE points SET pos = n FROM (SELECT id, row_number() OVER (ORDER BY p <-> spoint (0.2, 0.3)) n  FROM points ORDER BY p <-> spoint (0.2, 0.3) LIMIT 10) sel WHERE points.id = sel.id;
 SET enable_indexscan = false;
+SET enable_indexonlyscan = false;
+EXPLAIN (costs off) SELECT p <-> spoint (0.2, 0.3) FROM points ORDER BY 1 LIMIT 10;
 SELECT pos, row_number() OVER (ORDER BY p <-> spoint (0.2, 0.3)) n  FROM points ORDER BY p <-> spoint (0.2, 0.3) LIMIT 10;
 DROP TABLE points;
 
