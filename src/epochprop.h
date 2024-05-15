e1dee2488847482d9cee29b6ff07a5e127c1cedb
Markus Demleitner <m@tfiu.de>
2024-05-15 15:35:20 +0200

    Now more carefully nulling out null inputs in epoch_prop.
    This is so we do not invent values for pm, parallax, or rv.  We
    are actually a bit over-cautious and invalidate both PMs even if
    only one is missing.  This is because PMs mix, and hence there are traces
    of the invented 0 in the other component of the PM.  Similarly,
    when the parallax is missing or bad, the RV would be heavily contaminated;
    in these cases, we copy through the original RV, as it may still be useful
    and certainly will not be grossly wrong.
    
    (sorry about a few whitespace diffs introduced by killing trailing whitespace
    in sql/epochprop.sql and src/epochprop.c)

--- src/epochprop.h
+++ src/epochprop.h
@@ -6,15 +6,6 @@
 extern Datum epoch_prop(PG_FUNCTION_ARGS);
 
 
-/* a cartesian point; this is like geo_decl's point, but you can't
-have both geo_decls and pg_sphere right now (both define a type Point,
-not to mention they have different ideas on EPSILON */
-typedef struct s_cpoint
-{
-	double		x,
-				y;
-} CPoint;
-
 typedef struct s_phasevec
 {
 	SPoint		pos;			/* Position as an SPoint */
@@ -22,5 +13,5 @@ typedef struct s_phasevec
 								 * longitude has cos(lat) applied */
 	double		parallax;		/* in rad */
 	double		rv;				/* radial velocity in km/s */
-	int			parallax_valid; /* 1 if the parallax really is a NULL */
+	int			parallax_valid; /* 1 if we accept the parallax as physical */
 } phasevec;
