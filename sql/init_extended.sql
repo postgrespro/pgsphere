37897e98169c0e1fdcb8101764f095c94aa081a6
Vitaly Davydov <vitprof@gmail.com>
2024-11-07 12:47:14 +0300

    Add crushtest in build-and-check-windows-latest workflow

--- sql/init_extended.sql
+++ sql/init_extended.sql
@@ -2,15 +2,15 @@
 
 -- spoint_data and scircle_data tables have to be created and indexed using
 
-\! testsuite/gen_point.pl 1 > results/gen_point_1.sql
+\! perl testsuite/gen_point.pl 1 > results/gen_point_1.sql
 \i results/gen_point_1.sql
 
 -- and
 
-\! testsuite/gen_circle.pl 1 0.1 > results/gen_circle_1_0.1.sql
+\! perl testsuite/gen_circle.pl 1 0.1 > results/gen_circle_1_0.1.sql
 \i results/gen_circle_1_0.1.sql
 
 --
 
-\! testsuite/gen_poly.pl 1 0.1 4 > results/gen_poly_1_0.1_4.sql
+\! perl testsuite/gen_poly.pl 1 0.1 4 > results/gen_poly_1_0.1_4.sql
 \i results/gen_poly_1_0.1_4.sql
